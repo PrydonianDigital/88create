c0baa4b0ceba1a8b72f82d2cc8310eb0444413a9
Mark <vbloke@gmail.com>
2014-12-20 09:25:59 +0000

    Revert "lazyload"
    This reverts commit 3c4912114440c868061c366b0ff7ddef5a9b35c8.

--- archive-work.php
+++ archive-work.php
@@ -10,13 +10,13 @@ $background_image = get_background_image();
 	<div class="col-1-1">
 		<div id="filterlist"></div>
 		<ul id="filters" class="aligncenter">
-		    <li><a href="#" data-filter="*" class="selected">Everything</a></li>
+		    <li><a href="#Everything" data-filter="*" class="selected">Everything</a></li>
 			<?php 
 				$terms = get_terms('type');
 				$count = count($terms);
 				if ( $count > 0 ) {
 					foreach ( $terms as $term ) {
-						echo "<li><a href='#' data-show='".$term->slug."' data-filter='.".$term->slug."'>" . $term->name . "</a></li>\n";
+						echo "<li data-filter='.".$term->slug."'><a href='#" . $term->name . "' data-show='".$term->slug."'>" . $term->name . "</a></li>\n";
 					}
 				} 
 			?>
@@ -37,21 +37,21 @@ $background_image = get_background_image();
 		if ($work->have_posts()) : while ($work->have_posts()) : $work->the_post(); 
 		$terms = wp_get_post_terms( $work->post->ID, array( 'type' ) );
 	?>
-	<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'portfolio' ); $url = $thumb['0']; ?>
+	<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'work' ); $url = $thumb['0']; ?>
 	
 	<?php if ( 'video' == get_post_format() ) { ?>
 	
-		<div <?php post_class('col-2-3 portfolio'); ?>>
+		<div <?php post_class('col-2-3 portfolio'); ?> data-filters="<?php foreach ( $terms as $term ) : ?><?php echo $term->slug; ?><?php endforeach; ?>">
 			<?php $video = get_post_meta( $post->ID, '_cmb_v', true ); echo wp_oembed_get( $video ); ?>
 			<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 		</div>
 	
 	<?php } else { ?>
 	
-		<div <?php post_class('col-1-3 portfolio'); ?>>
+		<div <?php post_class('col-1-3 portfolio'); ?> data-filters="<?php foreach ( $terms as $term ) : ?><?php echo $term->slug; ?><?php endforeach; ?>">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="project">
 				<div class="thumbnail">
-					<img class="lazy" data-original="<?php echo $url; ?>"><i class="icon-zoomin"></i>
+					<?php the_post_thumbnail('portfolio lazy'); ?><i class="icon-zoomin"></i>
 				</div>
 			</a>
 		</div>
