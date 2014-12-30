49ca715e0d188a5df09ca07e90454f8fb3a6a67a
vbloke <vbloke@gmail.com>
2014-12-30 16:33:31 +0000

    contact page header

--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
-
+<style>
+	body {
+		background: <?php global $post; $colour = get_post_meta( $post->ID, '_cmb_colour', true ); echo $colour; ?> url(<?php global $post; $bg = get_post_meta( $post->ID, '_cmb_bg', true ); echo $bg; ?>);
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center top;
+		background-attachment: fixed;
+	}
+	header {
+		background: <?php global $post; $colour = get_post_meta( $post->ID, '_cmb_colour', true ); echo $colour; ?> url(<?php global $post; $bg = get_post_meta( $post->ID, '_cmb_bg', true ); echo $bg; ?>);
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center top;
+		background-attachment: fixed;
+	}
+</style>
 	<div id="map"></div>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
