1b39aee46ec49a591c46449352d94390a7fc4286
vbloke <vbloke@gmail.com>
2015-01-15 10:02:14 +0000

    footer date fix

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 
-
 <a href="#0" class="cd-top">Top</a>
 
 </div>
 
 <footer>
-	&copy; <?php the_date('Y'); ?> 88Create<br /><span>Part of the loveurope group of companies</span>
+	&copy; <?php echo date('Y'); ?> 88Create<br /><span>Part of the loveurope group of companies</span>
 </footer>
 
 <?php wp_footer(); ?>
