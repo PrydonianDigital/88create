899052313669bb5b47ee1f64e9e754643f4a92fc
vbloke <vbloke@gmail.com>
2015-01-15 10:58:53 +0000

    added browser update & map fix

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-
 <a href="#0" class="cd-top">Top</a>
 
 </div>
@@ -8,5 +7,17 @@
 </footer>
 
 <?php wp_footer(); ?>
+
+<script> 
+var $buoop = {vs:{i:10,f:25,o:12.1,s:6},c:2}; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
+</script> 
+
 </body>
 </html>
